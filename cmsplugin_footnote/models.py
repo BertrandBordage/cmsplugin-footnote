7dd933d2c0200c26c1a79e3d73a2d970e256a7d1
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-08-29 23:13:12 +0200

    Removes Footnote.__unicode__() (AbstractText.__unicode__ is almost the same).

--- cmsplugin_footnote/models.py
+++ cmsplugin_footnote/models.py
@@ -3,8 +3,6 @@
 from cms.plugins.text.models import AbstractText
 from django.db.models import CharField
 from django.utils.translation import ugettext_lazy as _
-from django.template.defaultfilters import truncatewords_html
-from django.utils.html import strip_tags
 from .utils import get_cache_key
 from django.core.cache import cache
 
@@ -16,9 +14,6 @@ class Footnote(AbstractText):
     class Meta:
         verbose_name = _('Footnote')
         verbose_name_plural = _('Footnotes')
-
-    def __unicode__(self):
-        return strip_tags(truncatewords_html(self.body, 5))
 
     def save(self, *args, **kwargs):
         super(Footnote, self).save(*args, **kwargs)
